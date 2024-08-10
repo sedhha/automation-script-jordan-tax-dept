1f821f600742ba5793955de9305d3022f3c79cd3
sedhha <activity.schoolsh2@gmail.com>
2024-08-10 16:50:29 +0530

    Keep process non exhausting

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import config from './config.json' assert { type: 'json' };
 import { main } from './modules/xml.js';
 import { logger } from './utils/logger.js';
 
-
 // Write logs every 30 seconds
 cron.schedule(config.cron_expression, () => {
-    main();
+    main()
+        .then(() => logger.info('Cron process success for this cycle.'))
+        .catch((err) => {
+            logger.error(`Cron process failed for this cycle. Error: ${err.message}`);
+        });
 });
 
-logger.info("Schedule started for " + config.cron_expression);
+logger.info('Schedule started for ' + config.cron_expression);
